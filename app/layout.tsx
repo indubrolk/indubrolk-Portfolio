0d0eaf9bb768abdd597ffd15f5d83321cc614c9d
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-04-12 09:32:11 +0530

    page.tsx edited

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Sora } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const font = Sora({
+    weight: ['100']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
