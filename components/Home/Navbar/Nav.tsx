a6d74b039bf689a775c543bee53f8f53f36c0352
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-05-03 14:15:36 +0530

    layout,nav updated

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Nav = () => {
     return(
         <div className="fixed h-[12vh] z-[10] bg-blue-950 w-full">
-        <div className="flex items-center h-full justify-between">
+        <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] ">
 
         </div>
         </div>
