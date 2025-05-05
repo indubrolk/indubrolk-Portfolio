ae5e73251845c9e2f4b97b680cdeb807ebfbeca7
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-05-06 00:43:21 +0530

    MobileNav.tsx,nav.tsx commited

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -8,7 +8,15 @@ const MobileNav = () => {
         {/* Nav Links */}
 
         <div className="text-white fixed justify-center flex flex-col h-full w-[80%] sm:w-[60%] bg-[#0f0715]
-        space-x-6 z-[1006]"></div>
+        space-x-6 z-[10000]">
+
+            {navLink.map((navLink) => {
+                return(
+                    <Link key={navLink.id} href={navLink.url}></Link>
+                )
+            })}
+
+        </div>
 
     </div>;
 
