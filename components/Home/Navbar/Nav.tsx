a8cb62e7079f256d7b6d1394f215f23fa6db27d7
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-05-05 13:10:57 +0530

    MobileNav.tsx,nav.tsx commited

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from 'react';
 
 const Nav = () => {
@@ -5,7 +6,13 @@ const Nav = () => {
         <div className="fixed h-[12vh] z-[10] bg-blue-950 w-full">
         <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
             {/* */}
-            <Image src=" "></Image>
+            <Image src="/images/logo2.png"
+                   alt="logo"
+                   width={170}
+                   height={170}
+                   className="ml-[-1.5rem] sm:ml-0"
+
+            ></Image>
         </div>
         </div>
 )
