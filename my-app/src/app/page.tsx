4323e9b277826385d3a70fabc43ac901100f48d7
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-11-17 15:29:02 +0530

    navbar updated

--- my-app/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-    return (
-      <div className='text-black'>Home Page</div>
-    );
-}
--- my-app/src/app/projects/page.tsx
+++
@@ -1,5 +0,0 @@
-//  const ProjectsPage = () => {
-//     return (
-//         <div>Contact Page</div>
-//     )
-// }
