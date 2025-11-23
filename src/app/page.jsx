7b61e0244c5bc63b3bbb50d73edead3834bf5b12
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-11-23 23:05:24 +0530

    Footer added to contents page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,7 @@ import DesktopWindow from "../components/DesktopWindow";
 import Dock from "../components/Dock";
 import { WordRotate } from "@/components/ui/word-rotate";
 import GradientText from "@/components/GradientText";
+import Footer from "@/components/Footer";
 
 
 const menuIcons = [
@@ -483,30 +484,32 @@ export default function HomePage() {
         </div>
       </main>
 
-      <footer className="relative z-10 mx-auto mt-6 flex max-w-6xl flex-col items-center gap-3 px-6 pb-24 text-center text-sm text-white/70">
-        <div className="h-px w-full max-w-4xl bg-white/10" />
-        <div className="flex flex-wrap items-center justify-center gap-4 text-xs uppercase tracking-[0.25em]">
-        <GradientText
-  colors={["#fd690c", "#d35101", "#ff893a", "#fdc19b"]}
-  animationSpeed={1}
-  showBorder={false}
-  className="custom-class"
->
-  Your Best Technocract!
-</GradientText>
-</div>
-        <div className="flex flex-wrap items-center justify-center gap-4 text-xs uppercase tracking-[0.25em] text-white/60">
-          {["Projects", "About", "Contact"].map((item) => (
-            <a key={item} href={`#${item.toLowerCase()}`} className="transition hover:text-white">
-              {item}
-            </a>
-          ))}
-        </div>
-        
-        <p className="text-xs text-white/60">
-          &copy; {new Date().getFullYear()} Induwara Jayakody - Crafted with Next.js & Tailwind.
-        </p>
-      </footer>
+        <Footer/>
+
+{/*      <footer className="relative z-10 mx-auto mt-6 flex max-w-6xl flex-col items-center gap-3 px-6 pb-24 text-center text-sm text-white/70">*/}
+{/*        <div className="h-px w-full max-w-4xl bg-white/10" />*/}
+{/*        <div className="flex flex-wrap items-center justify-center gap-4 text-xs uppercase tracking-[0.25em]">*/}
+{/*        <GradientText*/}
+{/*  colors={["#fd690c", "#d35101", "#ff893a", "#fdc19b"]}*/}
+{/*  animationSpeed={1}*/}
+{/*  showBorder={false}*/}
+{/*  className="custom-class"*/}
+{/*>*/}
+{/*  Your Best Technocract!*/}
+{/*</GradientText>*/}
+{/*</div>*/}
+{/*        <div className="flex flex-wrap items-center justify-center gap-4 text-xs uppercase tracking-[0.25em] text-white/60">*/}
+{/*          {["Projects", "About", "Contact"].map((item) => (*/}
+{/*            <a key={item} href={`#${item.toLowerCase()}`} className="transition hover:text-white">*/}
+{/*              {item}*/}
+{/*            </a>*/}
+{/*          ))}*/}
+{/*        </div>*/}
+{/*        */}
+{/*        <p className="text-xs text-white/60">*/}
+{/*          &copy; {new Date().getFullYear()} Induwara Jayakody - Crafted with Next.js & Tailwind.*/}
+{/*        </p>*/}
+{/*      </footer>*/}
       <Dock />
     </div>
   );
